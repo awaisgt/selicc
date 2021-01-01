cc444b03eb4373216e6a6918a18d32a50cc38ee3
awaisgt <39981782+awaisgt@users.noreply.github.com>
2021-01-01 18:55:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ import os
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
-def send_email(price,link,msgx):
+def send_email(price,link,msgx,emailto):
 	from email.mime.text import MIMEText
 	from email.mime.application import MIMEApplication
 	from email.mime.multipart import MIMEMultipart
 	from smtplib import SMTP
 	import smtplib
 	import sys
-	recipients = ['awaisghaffar77@gmail.com'] 
+	recipients = [emailto] 
 	emaillist = [elem.strip().split(',') for elem in recipients]
 	msg = MIMEMultipart()
-	msg['Subject'] = "pppppppppp" 
+	msg['Subject'] = "Price Update For " + str(msgx)
 	msg['From'] = "anything"
 
 	html = f"""\
@@ -67,15 +67,17 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 while(True):
 	driver.get("http://flasktestk.herokuapp.com/show")
-	link = driver.find_elements_by_class_name("username")[0]
+	link = driver.find_elements_by_class_name("link")[0]
 	temp = link.text
+	email = driver.find_elements_by_class_name("email")[0]
+	emailtemp = email.text
 	driver.get(link.text)
 	price = driver.find_elements_by_class_name("pdp-price")[0]
 	pricetext = price.text
 	name = driver.find_elements_by_class_name("pdp-mod-product-badge-title")[0]
 	#price_int = float(price.text[4:])
 	message =name.text
-	send_email(pricetext,temp,message)
+	send_email(pricetext,temp,message,emailtemp)
 	#s.sendmail("awaisghbh@gmail.com", "awaisghaffar77@gmail.com", message)
 	#s.quit()
 	print("ok")
